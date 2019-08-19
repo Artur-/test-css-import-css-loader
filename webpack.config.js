51b52a991af94f098fb8f86ddc6af4a35b21c736
Artur Signell <artur@vaadin.com>
2019-08-19 15:35:18 +0300

    Use css-loader

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,14 @@ const merge = require('webpack-merge');
 const flowDefaults = require('./webpack.generated.js');
 
 module.exports = merge(flowDefaults, {
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: ["css-loader"]
+			}
+		]
+	}
 
 });
 
