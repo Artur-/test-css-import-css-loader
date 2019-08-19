06ef26aea82a23999e2fefd6011f8e6dfcd7b74e
Artur Signell <artur@vaadin.com>
2019-08-19 15:34:46 +0300

    Make buttons red

--- src/main/java/com/example/app/MainView.java
+++ src/main/java/com/example/app/MainView.java
@@ -1,6 +1,7 @@
 package com.example.app;
 
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
@@ -11,6 +12,7 @@ import com.vaadin.flow.server.PWA;
  */
 @Route("")
 @PWA(name = "Project Base for Vaadin", shortName = "Project Base")
+@CssImport(value="test.css", themeFor="vaadin-button")
 public class MainView extends VerticalLayout {
 
     public MainView() {
